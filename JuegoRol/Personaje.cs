e442a4ad40984a85c0aecd70b1b30c629a90cf3c
Emilio Merchan <emimerchan83@gmail.com>
2021-06-03 17:52:53 -0300

    :C

--- JuegoRol/Personaje.cs
+++ JuegoRol/Personaje.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace JuegoRol
 {
-    class Personaje
+
+    public enum Tipos
+    {
+        PersonajeDelBosque,
+        Humano,
+        Elfo,
+        Orco,
+        Hobbit
+    }
+    public class Personaje
     {
 
         private string nombre;
-        private string tipo;
+        private Tipos tipo;
         private string apodo;
         private DateTime fechaDeNacimiento;
         private int edad;
@@ -21,15 +30,33 @@ namespace JuegoRol
         private int nivel;
         private int fuerza;
 
+        public Personaje(string nombre, Tipos tipo, string apodo, DateTime _fechaDeNacimiento, int edad)
+        {
+            Random random = new Random();
+            Nombre = nombre;
+            Salud = 100;
+            Tipo = tipo;
+            Apodo = apodo;
+            FechaDeNacimiento = _fechaDeNacimiento;
+            Edad = edad;
+            Velocidad = random.Next(1, 10);
+            Destreza = random.Next(1, 10);
+            Armadura = random.Next(1, 10);
+            Fuerza = random.Next(1, 10);
+            Nivel = 1;
+
+        }
+
         public int Velocidad { get => velocidad; set => velocidad = value; }
         public int Destreza { get => destreza; set => destreza = value; }
         public int Armadura { get => armadura; set => armadura = value; }
         public int Nivel { get => nivel; set => nivel = value; }
         public int Fuerza { get => fuerza; set => fuerza = value; }
         public string Nombre { get => nombre; set => nombre = value; }
-        public string Tipo { get => tipo; set => tipo = value; }
         public string Apodo { get => apodo; set => apodo = value; }
         public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
         public int Edad { get => edad; set => edad = value; }
+        public Tipos Tipo { get => tipo; set => tipo = value; }
+        public int Salud { get => salud; set => salud = value; }
     }
 }
